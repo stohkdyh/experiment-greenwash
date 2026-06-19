89103c955b9d028e46c2ee66c835de3b6298a67f
stohkdyh <adityaekapret@gmail.com>
2026-06-19 23:38:57 +0700

    feat: configure Laravel application bootstrap and Vercel serverless storage path mapping

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -25,3 +25,9 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
+
+if (isset($_ENV['APP_STORAGE'])) {
+    $app->useStoragePath($_ENV['APP_STORAGE']);
+}
+
+return $app;
