cdb3ec28967446350253b7c1db5250c63e5545d8
stohkdyh <adityaekapret@gmail.com>
2026-06-19 23:22:41 +0700

    feat: create users, password_reset_tokens, and sessions database tables

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,9 +14,15 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
+            $table->string('gender')->nullable();
+            $table->integer('age')->nullable();
+            $table->string('location')->nullable();
+            $table->string('occupation')->nullable();
+            $table->string('education')->nullable();
+            $table->string('income')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
